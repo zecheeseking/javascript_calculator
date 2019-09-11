a963ea7f3a888af07a46cf979ab7815882345792
Christopher Dale <zecheeseking@gmail.com>
2019-09-11 12:49:55 +0200

    created basic functions for operations.

--- calculator.js
+++ calculator.js
@@ -1,1 +1,26 @@
-console.log('Beep my jeep');
+console.log(operator(subtract, 5, 2));
+
+function operator(operator, a, b)
+{
+  return operator(a, b);
+}
+
+function add(a, b)
+{
+  return a + b;
+}
+
+function subtract(a, b)
+{
+  return a - b;
+}
+
+function multiply(a, b)
+{
+  return a * b;
+}
+
+function divide(a, b)
+{
+  return a / b;
+}
