85a58292175d6681d976dc16718c44e367ec03fc
Christopher Dale <zecheeseking@gmail.com>
2019-09-16 13:46:39 +0200

    Tokenization of equation added, added in blocking of trying to input 2 operators at once, changed typing of screen node to normal <p> tag, implemented new stylings to accommodate.

--- calculator.js
+++ calculator.js
@@ -1,7 +1,55 @@
-
-function operate(operator, a, b)
+var btns = document.querySelectorAll('input.display');
+for(let i = 0; i < btns.length; i++)
 {
-  return operator(a, b);
+    btns[i].addEventListener('click', (e) => appendToDisplay(e.target.value));
+}
+
+var clearbtn = document.querySelector('input[value=\'C\']');
+clearbtn.addEventListener('click', () => clearDisplay());
+
+var equalsbtn = document.querySelector('input[value=\'=\']');
+equalsbtn.addEventListener('click', () => operate());
+
+function appendToDisplay(str)
+{
+  var currText = document.querySelector('#screendisplay').textContent;
+  //check here for duplicate operations being added.
+  if(str.match(/\W/g) && currText.charAt(currText.length - 1).match(/\W/g)) return;
+
+  document.querySelector('#screendisplay').textContent = currText + str;
+}
+
+function clearDisplay()
+{
+  document.querySelector('#screendisplay').textContent = '';
+}
+
+function operate()
+{
+  Tokenize(document.querySelector('#screendisplay').textContent);
+}
+
+function Tokenize(equation)
+{
+  let index = 0;
+
+  while(equation.length)
+  {
+    if(equation[index].match(/\d/))
+    {
+      let token = equation.match(/\d+/);
+      console.log(token);
+      equation = equation.substr(token[0].length);
+      console.log(equation);
+    }
+    else if(equation[index].match(/\W/))
+    {
+      let token2 = equation.match(/\W+/);
+      console.log(token2);
+      equation = equation.substr(token2[0].length);
+      console.log(equation);
+    }
+  }
 }
 
 function add(a, b)
